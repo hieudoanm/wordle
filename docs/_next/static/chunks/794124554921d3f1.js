e665079956dff6f88158fe07c0c32715f9980259
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 19:57:16 +0700

    update head

--- docs/_next/static/chunks/794124554921d3f1.js
+++ docs/_next/static/chunks/794124554921d3f1.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/86bdaee5d59d828c.js",
+  "static/chunks/f90f207109d9087e.js",
   "static/chunks/6b06a7682729de4c.js",
   "static/chunks/f49d7083f01d3db6.js",
   "static/chunks/c536b4a9b8a1363f.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-57a79f9b923d7441.js"
+  "static/chunks/turbopack-83738f8f739432ae.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -19,6 +19,9 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <title>Wordle</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className={`${geistSans.className} ${geistMono.className}`}>
         <Component {...pageProps} />
