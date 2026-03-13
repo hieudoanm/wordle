871f4c2bb55a910669277bd6d37a007a726a8aa8
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-13 19:04:53 +0700

    add app page

--- docs/_next/static/chunks/a99099eaed95f898.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/f5e2d4748d397ca4.js",
-  "static/chunks/b580e8fb79b1abef.js",
-  "static/chunks/f49d7083f01d3db6.js",
-  "static/chunks/e04f22a616fa4d90.js",
-  "static/chunks/turbopack-9726cc8ff37a6ca3.js"
-])
--- docs/_next/static/zQHjS9j86F07z12J7qSg6/_buildManifest.js
+++ docs/_next/static/zQHjS9j86F07z12J7qSg6/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/a99099eaed95f898.js"
+    "static/chunks/a3649431a5976b5f.js"
   ],
   "/404": [
     "static/chunks/dfe335c59ab6178c.js"
@@ -10,6 +10,9 @@ self.__BUILD_MANIFEST = {
   ],
   "/_error": [
     "static/chunks/382648cdbfb2ee8e.js"
+  ],
+  "/app": [
+    "static/chunks/e662dd19498f25c9.js"
   ],
   "/version": [
     "static/chunks/b6076554521b6189.js"
@@ -25,6 +28,7 @@ self.__BUILD_MANIFEST = {
     "/500",
     "/_app",
     "/_error",
+    "/app",
     "/version"
   ]
 };self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
