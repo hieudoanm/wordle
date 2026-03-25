b6e2b2131b14d64b56f382f4a58b405b97099095
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-25 22:31:00 +0700

    Use HeadTemplate

--- docs/_next/static/chunks/791ae7abcf7f7e42.js
+++ docs/_next/static/chunks/791ae7abcf7f7e42.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/759aea7cef249632.js",
+  "static/chunks/b324e5e0875610d6.js",
   "static/chunks/e04f22a616fa4d90.js",
   "static/chunks/f49d7083f01d3db6.js",
   "static/chunks/db32cf447734db8c.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-44198cbb85727740.js"
+  "static/chunks/turbopack-e8831f955c27c872.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@wordle/styles/globals.css';
+import { HeadTemplate } from '../templates/HeadTemplate';
 import type { AppProps } from 'next/app';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Head from 'next/head';
 import { FC } from 'react';
 
 const geistSans = Geist({
@@ -17,12 +17,7 @@ const geistMono = Geist_Mono({
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>Wordle</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <HeadTemplate basic={{ title: 'Wordle' }} />
       <div className={`${geistSans.className} ${geistMono.className}`}>
         <Component {...pageProps} />
       </div>
